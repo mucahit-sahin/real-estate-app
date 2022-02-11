841ddb871eb5f353908110e8fbc5cef6f0eb4333
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-02-11 11:44:03 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="font-bold">Real Estate</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// color add
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tango: "#ed6b2d",
+      },
+    },
+  },
+  plugins: [],
+};
