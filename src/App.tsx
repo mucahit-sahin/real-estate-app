ca16ba5f23a24fb3aa0db653f7bd86f4a00baa3a
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-02-14 20:21:00 +0300

    Created Login Modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,18 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import { Categories } from "./components/Categories";
 import { FilterSearch } from "./components/FilterSearch";
 import { Footer } from "./components/Footer";
+import LoginModal from "./components/LoginModal";
 import { Map } from "./components/Map";
 import { Navbar } from "./components/Navbar";
+import RegisterModal from "./components/RegisterModal";
 import { RentoutBannerSM } from "./components/RentoutBannerSM";
 import { SearchLocation } from "./components/SearchLocation";
+import { useAppSelector } from "./store/hooks";
 
 function App() {
+  const { isLoginOpen, isSignupOpen } = useAppSelector((state) => state.modals);
   return (
-    <div>
+    <div className="flex flex-col ">
       <Navbar />
       <Routes>
         <Route
@@ -28,6 +32,7 @@ function App() {
                 <SearchLocation />
                 <RentoutBannerSM />
                 <Categories />
+                <Footer />
               </>
             }
           />
@@ -36,12 +41,20 @@ function App() {
             element={
               <>
                 <FilterSearch />
+                <div className="flex-1 flex  flex-row">
+                  <Map />
+                  <div className=" md:flex-2">List</div>
+                </div>
               </>
             }
           />
         </Route>
       </Routes>
-      <Footer />
+
+      {/* Login Modal */}
+      {isLoginOpen && <LoginModal />}
+      {/* Register Modal */}
+      {isSignupOpen && <RegisterModal />}
     </div>
   );
 }
