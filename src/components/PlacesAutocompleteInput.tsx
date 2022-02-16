ee7dc7795a2502aed2d93d64354bfd8ce0994ab9
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-02-16 12:19:54 +0300

    Created PlacesAutocompleteInput Component

---
+++ src/components/PlacesAutocompleteInput.tsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng,
+} from "react-places-autocomplete";
+import { useNavigate } from "react-router-dom";
+import { useAppDispatch } from "../store/hooks";
+import { setLocation } from "../store/slices/SearchLocationSlice";
+
+const PlacesAutocompleteInput = ({
+  className,
+  itemClass,
+  placeholder,
+  itemsClass,
+}: {
+  className: string;
+  itemClass: string;
+  placeholder: string;
+  itemsClass: string;
+}) => {
+  const [address, setAddress] = useState("");
+  const dispatch = useAppDispatch();
+  let navigate = useNavigate();
+
+  const handleSelect = async (address: string) => {
+    geocodeByAddress(address)
+      .then((results) => getLatLng(results[0]))
+      .then((latLng) => {
+        dispatch(setLocation({ address, lat: latLng.lat, lng: latLng.lng }));
+        navigate("/rent");
+      })
+      .catch((error) => console.error("Error", error));
+  };
+  return (
+    <PlacesAutocomplete
+      value={address}
+      onChange={setAddress}
+      onSelect={handleSelect}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div className="relative w-full">
+          <input
+            {...getInputProps({
+              placeholder: placeholder,
+              className: className,
+            })}
+          />
+          <div className={itemsClass}>
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: "#d1d1d1", cursor: "pointer" }
+                : { backgroundColor: "#ffffff", cursor: "pointer" };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    style,
+                  })}
+                  className={itemClass}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutocomplete>
+  );
+};
+
+export default PlacesAutocompleteInput;
