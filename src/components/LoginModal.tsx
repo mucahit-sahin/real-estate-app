b2b16ea553056f6cb372e8f11be20e3dded0c0d4
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-02-17 18:36:08 +0300

    Created Forms with React-hook-form

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,10 +1,22 @@
-import React from "react";
 import { Link } from "react-router-dom";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import { useAppDispatch } from "../store/hooks";
 import { closeLoginModal, openSignupModal } from "../store/slices/ModalSlice";
 
+type IFormInput = {
+  email: string;
+  password: string;
+};
+
 const LoginModal = () => {
   const dispatch = useAppDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   return (
     <div
@@ -26,48 +38,71 @@ const LoginModal = () => {
           </span>
         </div>
         {/* Form */}
-        <div className="flex flex-col px-4 py-2">
-          <div className="flex flex-col justify-between">
-            <span className="text-sm ">Email</span>
-            <input
-              type="email"
-              className="w-full rounded border border-gray-400 p-2"
-            />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col px-4 py-2">
+            <div className="flex flex-col justify-between">
+              <span className="text-sm ">Email</span>
+              <input
+                type="email"
+                className="w-full rounded border border-gray-400 p-2"
+                {...register("email", { required: "You must specify a email" })}
+              />
+              {errors.email && (
+                <span className="text-red-500 text-xs">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col justify-between my-2">
+              <span className="text-sm ">Password</span>
+              <input
+                type="password"
+                className="w-full rounded border border-gray-400 p-2"
+                {...register("password", {
+                  required: "You must specify a password",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="text-red-500 text-xs">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-row items-center justify-between">
+              <span className="text-base text-gray-400 flex items-center ">
+                <input type="checkbox" className="border border-gray-400 p-2" />
+                <span className="ml-2">Remember me</span>
+              </span>
+              <Link to="" className="text-base text-gray-400">
+                Forgot password?
+              </Link>
+            </div>
+            <div className="mt-4 flex flex-row items-center justify-between">
+              <input
+                type="submit"
+                value="Login"
+                className="bg-tango w-full text-white font-bold py-2 px-4 rounded"
+              />
+            </div>
+
+            {/* Signup Link */}
+            <div className="flex flex-row items-center justify-between mt-4">
+              <span className="text-base text-gray-400">
+                Don't have an account?
+              </span>
+              <button
+                className="text-base text-gray-400"
+                onClick={() => dispatch(openSignupModal())}
+              >
+                Signup
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col justify-between my-2">
-            <span className="text-sm ">Password</span>
-            <input
-              type="password"
-              className="w-full rounded border border-gray-400 p-2"
-            />
-          </div>
-          <div className="flex flex-row items-center justify-between">
-            <span className="text-base text-gray-400 flex items-center ">
-              <input type="checkbox" className="border border-gray-400 p-2" />
-              <span className="ml-2">Remember me</span>
-            </span>
-            <Link to="" className="text-base text-gray-400">
-              Forgot password?
-            </Link>
-          </div>
-          <div className="mt-4 flex flex-row items-center justify-between">
-            <button className="bg-tango w-full text-white font-bold py-2 px-4 rounded">
-              Login
-            </button>
-          </div>
-          {/* Signup Link */}
-          <div className="flex flex-row items-center justify-between mt-4">
-            <span className="text-base text-gray-400">
-              Don't have an account?
-            </span>
-            <button
-              className="text-base text-gray-400"
-              onClick={() => dispatch(openSignupModal())}
-            >
-              Signup
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
 import { useAppDispatch } from "../store/hooks";
 import { closeSignupModal, openLoginModal } from "../store/slices/ModalSlice";
 
+interface IFormInput {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  phoneNumber: string;
+  fullname: string;
+  aggrement: boolean;
+}
+
 const RegisterModal = () => {
   const dispatch = useAppDispatch();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   return (
     <div
@@ -25,53 +43,117 @@ const RegisterModal = () => {
           </span>
         </div>
         {/* Form */}
-        <div className="flex flex-col px-4 py-2">
-          <div className="flex flex-col justify-between">
-            <span className="text-sm ">Full Name</span>
-            <input
-              type="text"
-              className="w-full rounded border border-gray-400 p-2"
-            />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col px-4 py-2">
+            <div className="flex flex-col justify-between">
+              <span className="text-sm ">Full Name</span>
+              <input
+                type="text"
+                className="w-full rounded border border-gray-400 p-2"
+                {...register("fullname", {
+                  required: "You must specify a password",
+                })}
+              />
+              {errors.fullname && (
+                <span className="text-red-500 text-xs">
+                  {errors.fullname.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col justify-between">
+              <span className="text-sm ">Email</span>
+              <input
+                type="email"
+                className="w-full rounded border border-gray-400 p-2"
+                {...register("email", {
+                  required: "You must specify a password",
+                })}
+              />
+              {errors.email && (
+                <span className="text-red-500 text-xs">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col justify-between my-2">
+              <span className="text-sm ">Password</span>
+              <input
+                type="password"
+                className="w-full rounded border border-gray-400 p-2"
+                {...register("password", {
+                  required: "You must specify a password",
+                  minLength: {
+                    value: 8,
+                    message: "Password must have at least 8 characters",
+                  },
+                })}
+              />
+              {errors.password && (
+                <span className="text-red-500 text-xs">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+            <div className="flex flex-col justify-between my-2">
+              <span className="text-sm ">Confirm Password</span>
+              <input
+                type="password"
+                className="w-full rounded border border-gray-400 p-2"
+                {...register("confirmPassword", {
+                  required: "You must specify a password",
+                  minLength: {
+                    value: 8,
+                    message: "Password must have at least 8 characters",
+                  },
+                  validate: (value) =>
+                    value === watch("password", "") || "Passwords do not match",
+                })}
+              />
+              {errors.confirmPassword && (
+                <span className="text-red-500 text-xs">
+                  {errors.confirmPassword.message}
+                </span>
+              )}
+            </div>
+            {/* Aggrement */}
+            <div className="flex flex-col  justify-between">
+              <span className="text-sm text-gray-400 flex flex-row items-center ">
+                <input
+                  type="checkbox"
+                  className="border border-gray-400 p-2"
+                  {...register("aggrement", {
+                    required: "You must agree to the terms and conditions",
+                  })}
+                />
+                <span className="ml-2">
+                  I agree to the terms and conditions
+                </span>
+              </span>
+              {errors.aggrement && (
+                <span className="text-red-500 text-xs">
+                  {errors.aggrement.message}
+                </span>
+              )}
+            </div>
+            <div className="mt-4 flex flex-row items-center justify-between">
+              <button className="bg-tango w-full text-white font-bold py-2 px-4 rounded">
+                Register
+              </button>
+            </div>
+            {/* Signup Link */}
+            <div className="flex flex-row items-center justify-between mt-4">
+              <span className="text-base text-gray-400">
+                Already have an account?
+              </span>
+              <button
+                className="text-base text-gray-400"
+                onClick={() => dispatch(openLoginModal())}
+              >
+                Login
+              </button>
+            </div>
           </div>
-          <div className="flex flex-col justify-between">
-            <span className="text-sm ">Email</span>
-            <input
-              type="email"
-              className="w-full rounded border border-gray-400 p-2"
-            />
-          </div>
-          <div className="flex flex-col justify-between my-2">
-            <span className="text-sm ">Password</span>
-            <input
-              type="password"
-              className="w-full rounded border border-gray-400 p-2"
-            />
-          </div>
-          {/* Aggrement */}
-          <div className="flex flex-row items-center justify-between">
-            <span className="text-sm text-gray-400 flex items-center ">
-              <input type="checkbox" className="border border-gray-400 p-2" />
-              <span className="ml-2">I agree to the terms and conditions</span>
-            </span>
-          </div>
-          <div className="mt-4 flex flex-row items-center justify-between">
-            <button className="bg-tango w-full text-white font-bold py-2 px-4 rounded">
-              Register
-            </button>
-          </div>
-          {/* Signup Link */}
-          <div className="flex flex-row items-center justify-between mt-4">
-            <span className="text-base text-gray-400">
-              Already have an account?
-            </span>
-            <button
-              className="text-base text-gray-400"
-              onClick={() => dispatch(openLoginModal())}
-            >
-              Login
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
