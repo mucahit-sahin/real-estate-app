5e688dd5be5dadda03a7e7c1b2e09f00217c3ef8
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-02-26 23:09:22 +0300

    Created propert types

---
+++ src/types/propertyTypes.ts
@@ -0,0 +1,29 @@
+export interface PropertyState {
+    properties: Property[];
+    property: Property;
+    loading: boolean;
+    error: string | null;
+}
+
+export interface Property {
+    title: string;
+    description: string;
+    price: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    city: string;
+    state: string;
+    area: string;
+    country: string;
+    zipCode: string;
+    propertyType: string;
+    squareMeters: string;
+    bedrooms: string;
+    bathrooms: string;
+    latitude: number;
+    longitude: number;
+    isDogfriendly: boolean;
+    isCatfriendly: boolean;
+    isSmokingfriendly: boolean;
+}
