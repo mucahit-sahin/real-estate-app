102b5f4320246900e819200b8c38c451d63d5bee
Mücahit Şahin <mucahitsahin786@gmail.com>
2022-02-28 23:14:29 +0300

    Register Modal View changed

--- src/components/RegisterModal.tsx
+++ src/components/RegisterModal.tsx
@@ -40,131 +40,140 @@ const RegisterModal = () => {
       onClick={() => dispatch(closeSignupModal())}
     >
       <div
-        className="bg-white absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col w-96 rounded shadow-lg shadow-gray-500 z-30"
+        className=" absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col w-96 rounded shadow-lg shadow-gray-500 z-30"
         onClick={(e) => e.stopPropagation()}
       >
-        {/* Title */}
-        <div className="flex flex-row items-center justify-between px-4 py-2">
-          <span className="text-2xl font-bold ">Register</span>
-          <span
-            className="text-2xl font-bold cursor-pointer w-8 h-8 flex items-center justify-center rounded hover:bg-gray-500 hover:bg-opacity-10"
-            onClick={() => dispatch(closeSignupModal())}
-          >
-            &times;
-          </span>
-        </div>
-        {/* Form */}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-col px-4 py-2">
-            <div className="flex flex-col justify-between">
-              <span className="text-sm ">Full Name</span>
-              <input
-                type="text"
-                className="w-full rounded border border-gray-400 p-2"
-                {...register("fullname", {
-                  required: "You must specify a password",
-                })}
-              />
-              {errors.fullname && (
-                <span className="text-red-500 text-xs">
-                  {errors.fullname.message}
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col justify-between">
-              <span className="text-sm ">Email</span>
-              <input
-                type="email"
-                className="w-full rounded border border-gray-400 p-2"
-                {...register("email", {
-                  required: "You must specify a password",
-                })}
-              />
-              {errors.email && (
-                <span className="text-red-500 text-xs">
-                  {errors.email.message}
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col justify-between my-2">
-              <span className="text-sm ">Password</span>
-              <input
-                type="password"
-                className="w-full rounded border border-gray-400 p-2"
-                {...register("password", {
-                  required: "You must specify a password",
-                  minLength: {
-                    value: 8,
-                    message: "Password must have at least 8 characters",
-                  },
-                })}
-              />
-              {errors.password && (
-                <span className="text-red-500 text-xs">
-                  {errors.password.message}
-                </span>
-              )}
-            </div>
-            <div className="flex flex-col justify-between my-2">
-              <span className="text-sm ">Confirm Password</span>
-              <input
-                type="password"
-                className="w-full rounded border border-gray-400 p-2"
-                {...register("confirmPassword", {
-                  required: "You must specify a password",
-                  minLength: {
-                    value: 8,
-                    message: "Password must have at least 8 characters",
-                  },
-                  validate: (value) =>
-                    value === watch("password", "") || "Passwords do not match",
-                })}
-              />
-              {errors.confirmPassword && (
-                <span className="text-red-500 text-xs">
-                  {errors.confirmPassword.message}
-                </span>
-              )}
-            </div>
-            {/* Aggrement */}
-            <div className="flex flex-col  justify-between">
-              <span className="text-sm text-gray-400 flex flex-row items-center ">
-                <input
-                  type="checkbox"
-                  className="border border-gray-400 p-2"
-                  {...register("aggrement", {
-                    required: "You must agree to the terms and conditions",
-                  })}
-                />
-                <span className="ml-2">
-                  I agree to the terms and conditions
-                </span>
-              </span>
-              {errors.aggrement && (
-                <span className="text-red-500 text-xs">
-                  {errors.aggrement.message}
-                </span>
-              )}
-            </div>
-            <div className="mt-4 flex flex-row items-center justify-between">
-              <button className="bg-tango w-full text-white font-bold py-2 px-4 rounded">
-                Register
-              </button>
-            </div>
-            {/* Signup Link */}
-            <div className="flex flex-row items-center justify-between mt-4">
-              <span className="text-base text-gray-400">
-                Already have an account?
-              </span>
-              <button
-                className="text-base text-gray-400"
-                onClick={() => dispatch(openLoginModal())}
-              >
-                Login
-              </button>
-            </div>
+        <div className="w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+          <div className="px-6 py-4">
+            {/* Title */}
+            <h2 className="text-3xl font-bold text-center text-gray-700 dark:text-white">
+              Register
+            </h2>
+            {/* Form */}
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div className="flex flex-col">
+                <div className="flex flex-col justify-between my-2">
+                  <span className="block text-sm text-gray-800 dark:text-gray-200">
+                    Full Name
+                  </span>
+                  <input
+                    type="text"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    {...register("fullname", {
+                      required: "You must specify a password",
+                    })}
+                  />
+                  {errors.fullname && (
+                    <span className="text-red-500 text-xs">
+                      {errors.fullname.message}
+                    </span>
+                  )}
+                </div>
+                <div className="flex flex-col justify-between my-2">
+                  <span className="block text-sm text-gray-800 dark:text-gray-200">
+                    Email
+                  </span>
+                  <input
+                    type="email"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    {...register("email", {
+                      required: "You must specify a password",
+                    })}
+                  />
+                  {errors.email && (
+                    <span className="text-red-500 text-xs">
+                      {errors.email.message}
+                    </span>
+                  )}
+                </div>
+                <div className="flex flex-col justify-between my-2">
+                  <span className="block text-sm text-gray-800 dark:text-gray-200">
+                    Password
+                  </span>
+                  <input
+                    type="password"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    {...register("password", {
+                      required: "You must specify a password",
+                      minLength: {
+                        value: 8,
+                        message: "Password must have at least 8 characters",
+                      },
+                    })}
+                  />
+                  {errors.password && (
+                    <span className="text-red-500 text-xs">
+                      {errors.password.message}
+                    </span>
+                  )}
+                </div>
+                <div className="flex flex-col justify-between my-2">
+                  <span className="block text-sm text-gray-800 dark:text-gray-200">
+                    Confirm Password
+                  </span>
+                  <input
+                    type="password"
+                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                    {...register("confirmPassword", {
+                      required: "You must specify a password",
+                      minLength: {
+                        value: 8,
+                        message: "Password must have at least 8 characters",
+                      },
+                      validate: (value) =>
+                        value === watch("password", "") ||
+                        "Passwords do not match",
+                    })}
+                  />
+                  {errors.confirmPassword && (
+                    <span className="text-red-500 text-xs">
+                      {errors.confirmPassword.message}
+                    </span>
+                  )}
+                </div>
+                {/* Aggrement */}
+                <div className="flex flex-col  justify-between">
+                  <span className="text-sm text-gray-400 flex flex-row items-center ">
+                    <input
+                      type="checkbox"
+                      className="border border-gray-400 p-2"
+                      {...register("aggrement", {
+                        required: "You must agree to the terms and conditions",
+                      })}
+                    />
+                    <span className="ml-2">
+                      I agree to the terms and conditions
+                    </span>
+                  </span>
+                  {errors.aggrement && (
+                    <span className="text-red-500 text-xs">
+                      {errors.aggrement.message}
+                    </span>
+                  )}
+                </div>
+                <div className="mt-4 flex flex-row items-center justify-between">
+                  <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                    Register
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
+
+          {/* Signup Link */}
+          <div className="flex items-center justify-center py-4 text-center bg-gray-50 dark:bg-gray-700">
+            <span className="text-sm text-gray-600 dark:text-gray-200">
+              Already have an account?
+            </span>
+
+            <button
+              onClick={() => dispatch(openLoginModal())}
+              className="mx-2 text-sm font-bold text-blue-500 dark:text-blue-400 hover:underline"
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
